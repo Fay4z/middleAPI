fb7f81f801a4c0d74b9ad51b5bfd8ae576960134
Fay4z <fayazjr212@gmail.com>
2026-01-31 21:40:36 +0530

    inital frontend setup finished

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <p className=" text-xl">The app is working</p>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
